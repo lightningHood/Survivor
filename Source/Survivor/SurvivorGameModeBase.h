48806fa61768ef58bc13d6f4f35bc2291c31a307
lightningHood <alphabash@gmail.com>
2023-10-18 08:15:24 -0400

    Initial commit

---
+++ Source/Survivor/SurvivorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVOR_API ASurvivorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
